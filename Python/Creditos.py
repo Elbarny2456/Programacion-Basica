40153d542b960eb76e55c4bab69cadc87fad66e4
Elbarny2456 <L241050123@pabellon.tecnm.mx>
2025-02-27 07:38:40 -0600

    creditos 27 de febrero
    ea

---
+++ Python/Creditos.py
@@ -0,0 +1,9 @@
+Creditos=int(input("Ingrese el total de sus creditos:"))
+if Creditos >= 80:
+    print("Puedes hacer serivicio social")
+else:
+    print("No puedes hacer servicio social")
+if Creditos >= 120:
+    print("Puedes hacer residencias")
+else:
+    print("No puedes hacer residencias")
