bea09f23b124ba1355ee6f23235af18b894173d3
Elbarny2456 <L241050123@pabellon.tecnm.mx>
2025-03-10 09:36:10 -0600

    hackaton
    ea

---
+++ UNIDAD3/hackaton.py
@@ -0,0 +1,14 @@
+
+lista_compras=int(input("desea agregar mandado si=1 no=0"))
+if lista_compras >= 0:
+    print("va agregue sus productos")
+else:
+    print("Nimodo")
+producto1 = input("Agrega el primer producto: ")
+lista_compras1(producto1)
+
+producto2 = input("Agrega el segundo producto: ")
+lista_compras1(producto2)
+
+producto3 = input("Agrega el tercer producto: ")
+lista_compras1(producto3)
