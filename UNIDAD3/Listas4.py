0a2604ce8117e092777475cefca7d1202533a9d2
Elbarny2456 <L241050123@pabellon.tecnm.mx>
2025-03-11 07:58:33 -0600

    listas4

---
+++ UNIDAD3/Listas4.py
@@ -0,0 +1,17 @@
+
+nombres = ["Sabayd", "luis", "Lupita", "victor", "Roberto", "cristian"]
+edades = [18, 20, 30, 18, 19, 22, 24, 27]
+Libre = ["caguama", 3, 4, "1 kilo de azucar", 54, "agua de melon"]
+Materias = ["Programacion basica", "Calculo integral", "Ciencias de los materiales", "Algebra Lineal", "Contabilidad y administracion" "Estadistica y control"]
+Profes = ["Eduardo Flores", "Marlem solis", "Enrique"]
+print(len(nombres))#es para contar el numero de articulos en la lista
+print(max(edades))#el numero max
+print(min(edades))#el numero minimo
+edades.sort()#ordenar menor a mayor y orden algebraico
+print(edades)
+Materias.remove("Programacion basica")#removeralgo de la lista
+Materias.append("Programacion Avanzada")
+print(Materias)
+print(Libre)
+print(Materias)
+print(Profes)
