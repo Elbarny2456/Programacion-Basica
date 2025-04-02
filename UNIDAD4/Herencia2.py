7b85c3b543f6bb10643a31978ebab543f16c5a7c
Elbarny2456 <L241050123@pabellon.tecnm.mx>
2025-04-02 07:59:02 -0600

    herencia 2
    ea

--- UNIDAD4/Herencia2.py
+++ UNIDAD4/Herencia2.py
@@ -1,41 +1,38 @@
-# Herencia
 # Clase base o padre
 class Animal:
-    def __init__(self, nombre, raza):
+    def __init__(self, nombre, raza, especie, color):
         self.nombre = nombre
         self.raza = raza
-   
-        self.color = color
-        self.comer = comer
         self.especie = especie
+        self.color = color
 
     def presentarse(self):
-        return f"Hola, me llamo {self.nombre} y mi especie es {self.especie} ."
+        return f"Hola, me llamo {self.nombre}, soy de la especie {self.especie}, de raza {self.raza} y de color {self.color}."
 
 # Clase derivada o hija
 class Perro(Animal):
-    def __init__(self, nombre, especie):
-        super().__init__(nombre, especie)  # Llamada al constructor de la clase padre
+    def __init__(self, nombre, raza, especie, color, ladrar):
+        super().__init__(nombre, raza, especie, color)  # Llamada al constructor de la clase padre
         self.ladrar = ladrar
 
     def presentarse(self):
-        # Sobrescribimos el método de la clase padre
-        return f"Hola, me llamo {self.nombre}, mi especie es {self.especie} y el sonido que yo hago es {self.ladrar}."
+       
+        return f"Hola, me llamo {self.nombre}, soy de la especie {self.especie}, de raza {self.raza}, hago el sonido {self.ladrar}."
 
 # Otra clase derivada
 class Gato(Animal):
-    def __init__(self, nombre, especie):
-        super().__init__(nombre, especie)
+    def __init__(self, nombre, raza, especie, color, maullar):
+        super().__init__(nombre, raza, especie, color)
         self.maullar = maullar
 
     def presentarse(self):
-        return f"Hola, me llamo {self.nombre}, mi especie es {self.especie} y el sonido que yo hago {self.maullar}."
+        return f"Hola, me llamo {self.nombre}, soy de la especie {self.especie}, de raza {self.raza}, hago el sonido {self.maullar}."
 
 # Programa principal
 if __name__ == "__main__":
-    animal = Animal("animal",50)
-    perro = Perro("PERRO", vivipeda, "Ladrar")
-    gato = Gato("Gato", vivipeda, "maullar")
+    animal = Animal("Animal genérico", "Desconocida", "Desconocida", "Gris")
+    perro = Perro("Firulais", "Chihuahua", "Canino", "Blanco", "Guau guau")
+    gato = Gato("Michi", "Siames", "Felino", "Negro", "Miau")
 
     print(animal.presentarse())
     print(perro.presentarse())
