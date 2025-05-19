d873e975264b7da39e5ae2f2c93ad5c3413c55b2
Elbarny2456 <L241050123@pabellon.tecnm.mx>
2025-05-19 08:11:35 -0600

    pseudocodigo
    ea

--- UNIDAD6/Pseudocodigo1.py
+++ UNIDAD6/Pseudocodigo1.py
@@ -1,7 +1,7 @@
 def contar_palabras():
     nombre_programa = input("introduce el nombre del programa:")
     print("has iniciado el programa", nombre_programa)
-    texto_usuario = input("escribe una frase o texto")
+    texto_usuario = input("escribe una frase o texto:")
     cantidad_palabras = len(texto_usuario.split())
     print("el texto contiene", cantidad_palabras, "palabras")
 contar_palabras()
